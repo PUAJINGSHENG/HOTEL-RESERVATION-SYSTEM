9947dcbdb91de139629effca0fa61ff5cb7c80b9
PUAJINGSHENG <102914802+PUAJINGSHENG@users.noreply.github.com>
2023-01-12 02:36:21 +0800

    Update index.php

--- admin/mod_reports/index.php
+++ admin/mod_reports/index.php
@@ -5,17 +5,8 @@ require_once("../../includes/initialize.php");
  }
 $view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
 $title ="Report";
-switch ($view) {
-	case 'list' :
-		$content    = 'list.php';		
-		break;
-	case 'list' :
-		$content    = 'list.php';		
-		break;	
-			
-	default :
-		$content    = 'list.php';		
-}
+$content    = 'list.php';		
+		
   include '../modal.php';
 require_once '../themes/backendTemplate.php';
 ?>
