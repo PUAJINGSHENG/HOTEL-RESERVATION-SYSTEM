1f349b88bd8e12e7d4199a4deb21d91b32021d04
PUAJINGSHENG <102914802+PUAJINGSHENG@users.noreply.github.com>
2023-01-15 19:03:03 +0800

    Update search.php
    I had changed with the <th> tag.

--- app/admin/mod_reports/search.php
+++ app/admin/mod_reports/search.php
@@ -2,7 +2,7 @@
 <div class="container">
 	<div class="panel ">
 		<div class="panel-body">
-		     
+
 		<form class="form-inline" action="" method="post">
 		 <div class="form-group">
 		 <input class="form-control" size="20" type="text" value="<?php echo isset($_POST['txtsearch']) ? $_POST['txtsearch'] :'' ?>" Placeholder="Search For...." name="txtsearch" id="txtsearch">
@@ -29,10 +29,10 @@
 		    <label class="sr-only" for="exampleInputPassword2">Check Out:</label>
 		      <input class="form-control date end " size="20" type="text" value="<?php echo (isset($_POST['end'])) ? $_POST['end'] : date('Y-m-d'); ?>"  name="end" id="end" data-date="" data-date-format="yyyy-mm-dd" data-link-field="any" data-link-format="yyyy-mm-dd">
 		  </div>
-		  
+
 		  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 		</form>
-	
+
 
 
 <form  method="post" action="list.php" target="_blank">
@@ -42,23 +42,23 @@
 <thead>
 <tr><td colspan="9" align="center"><h1 class="page-header">Monbela Tourist Inn</h1></td></tr>
 <tr bgcolor="#999999">
-<td ><strong>Guest</strong></td>
-<td ><strong>Arrival</strong></td>
-<td ><strong>Departure</strong></td>
-<td ><strong>Room</strong></td>
-<td ><strong>Price</strong></td>
-<td ><strong>Nights</strong></td>
-<td ><strong>Subtotal</strong></td>
-<td ><strong>Status</strong></td>
+<th >Guest</th>
+<th >Arrival</th>
+<th >Departure</th>
+<th >Room</th>
+<th >Price</th>
+<th >Nights</th>
+<th >Subtotal</th>
+<th >Status</th>
 </tr>
 </thead>
-<tbody>		
+<tbody>
 <?php
 if(isset($_POST['submit'])){
 	// $_SESSION['start']=$_POST['start'];
-	// $_SESSION['end']=$_POST['end'];	
+	// $_SESSION['end']=$_POST['end'];
 	$sql ="SELECT  `G_FNAME`, `G_LNAME`,`ARRIVAL`, `DEPARTURE`,`ROOM`,`ROOMDESC` ,`PRICE` ,`RPRICE`,`STATUS`
-	 FROM `tblguest` G,`tblreservation` R, `tblroom` RR 
+	 FROM `tblguest` G,`tblreservation` R, `tblroom` RR
 	 WHERE  G.`GUESTID`=R.`GUESTID` AND R.`ROOMID`=RR.`ROOMID` AND DATE(`ARRIVAL`) >=  '".$_POST['start']."'
 	AND DATE(`DEPARTURE`) <=  '".$_POST['end']."' AND STATUS='" .$_POST['categ']."' ";
 	$mydb->setQuery($sql);
@@ -81,12 +81,12 @@ if(isset($_POST['submit'])){
 				<td><?php echo $result->STATUS; ?></td>
 				</tr>
 
-			<?php 
+			<?php
 			@$tot += $result->RPRICE;
 
 
 		}
-			
+
 		}else{
 
 		echo '<tr><td colspan="8" align="center"><h2>Please Enter Then Dates</h2></td></tr>';
@@ -104,26 +104,26 @@ if(isset($_POST['submit'])){
 <input type="button" value="Print Report" onclick="tablePrint();" class="btn btn-primary">
 </form>
 </div>
-</div> 
+</div>
 
 <script>
-function tablePrint(){  
-    var display_setting="toolbar=no,location=no,directories=no,menubar=no,";  
-    display_setting+="scrollbars=no,width=500, height=500, left=100, top=25";  
-    var content_innerhtml = document.getElementById("printout").innerHTML;  
-    var document_print=window.open("","",display_setting);  
-    document_print.document.open();  
-    document_print.document.write('<body style="font-family:verdana; font-size:12px;" onLoad="self.print();self.close();" >');  
-    document_print.document.write(content_innerhtml);  
-    document_print.document.write('</body></html>');  
-    document_print.print();  
-    document_print.document.close();  
-    return false;  
-    } 
+function tablePrint(){
+    var display_setting="toolbar=no,location=no,directories=no,menubar=no,";
+    display_setting+="scrollbars=no,width=500, height=500, left=100, top=25";
+    var content_innerhtml = document.getElementById("printout").innerHTML;
+    var document_print=window.open("","",display_setting);
+    document_print.document.open();
+    document_print.document.write('<body style="font-family:verdana; font-size:12px;" onLoad="self.print();self.close();" >');
+    document_print.document.write(content_innerhtml);
+    document_print.document.write('</body></html>');
+    document_print.print();
+    document_print.document.close();
+    return false;
+    }
 	$(document).ready(function() {
 		oTable = jQuery('#list').dataTable({
 		"bJQueryUI": true,
 		"sPaginationType": "full_numbers"
 		} );
-	});		
+	});
 </script>
