b49517963ee021c92e245bce6173d8131326c618
Thaya19 <103048952+Thaya19@users.noreply.github.com>
2023-01-16 16:26:33 +0800

    Change "<td>" into "<th>" for table header.
    I had changed table header with the table header tag

--- app/admin/mod_reservation/list.php
+++ app/admin/mod_reservation/list.php
@@ -1,6 +1,6 @@
 <?php
 		check_message();
-			
+
 		?>
 <div class="container">
 <!-- <div class="panel panel-primary"> -->
@@ -9,40 +9,40 @@
 	<table id="table" class="table table-striped" cellspacing="0">
 <thead>
 <tr>
-<td width="5%">#</td>	
+<td width="5%">#</td>
 
-<td width="90"><strong>Guest</strong></td>
+<th width="90">Guest</th>
 <!--<td width="10"><strong>Confirmation</strong></td>-->
-<td width="80"><strong>Transaction Date</strong></td>
-<td width="80"><strong>Confimation Code</strong></td>
-<td width="70"><strong>Total Rooms</strong></td>
-<td width="80"><strong>Total Price</strong></td>
+<th width="80">Transaction Date</th>
+<th width="80">Confimation Code</th>
+<th width="70">Total Rooms</td>
+<th width="80">Total Price</td>
 <!-- <td width="80"><strong>Nights</strong></td> -->
-<td width="80"><strong>Status</strong></td>
-<td width="100"><strong>Action</strong></td>
+<th width="80">Status</td>
+<th width="100">Action</td>
 </tr>
 </thead>
 <tbody>
 <?php
- 
+
 $mydb->setQuery("SELECT  p.`GUESTID`,`G_FNAME` ,  `G_LNAME` ,  `G_ADDRESS` ,  `TRANSDATE` ,  `CONFIRMATIONCODE` ,  `PQTY` ,  `SPRICE` ,`STATUS`
 				FROM  `tblpayment` p,  `tblguest` g
-				WHERE p.`GUESTID` = g.`GUESTID`   
+				WHERE p.`GUESTID` = g.`GUESTID`
 				ORDER BY p.`STATUS`='pending' desc ");
 $cur = $mydb->loadResultList();
-				  			 
+
 foreach ($cur as $result) {
 ?>
 <tr>
 <td width="5%" align="center"></td>
 <td><?php echo $result->G_FNAME." ".$result->G_LNAME; ?></td>
-<td><?php echo $result->TRANSDATE; ?></td>  
+<td><?php echo $result->TRANSDATE; ?></td>
 <td><?php echo $result->CONFIRMATIONCODE; ?></td>
 <td><?php echo $result->PQTY; ?></td>
 <td>&euro;<?php echo $result->SPRICE; ?></td>
-<td><?php echo $result->STATUS; ?></td> 
+<td><?php echo $result->STATUS; ?></td>
  <td >
-	<?php 
+	<?php
 		if($result->STATUS == 'Confirmed'){ ?>
 		<!-- <a class="cls_btn" id="<?php echo $result->reservation_id; ?>" data-toggle='modal' href="#profile" title="Click here to Change Image." ><i class="icon-edit">test</a> -->
 			<a href="index.php?view=view&code=<?php echo $result->CONFIRMATIONCODE; ?>" class="btn btn-primary btn-xs" ><i class="icon-edit">View</a>
@@ -62,7 +62,7 @@ foreach ($cur as $result) {
 			<a href="index.php?view=view&code=<?php echo $result->CONFIRMATIONCODE; ?>" class="btn btn-primary btn-xs" ><i class="icon-edit">View</a>
 			<a href="controller.php?action=delete&code=<?php echo $result->CONFIRMATIONCODE; ?>" class="btn btn-danger btn-xs"  ><i class="icon-edit">Delete</a>
 			<a href="index.php?view=edit&code=<?php echo $result->CONFIRMATIONCODE; ?>&id=<?php echo $result->GUESTID; ?>" class="btn btn-primary btn-xs"  ><i class="icon-edit">Edit</a>
-			
+
 	<?php }else{
 			?>
 			<a href="index.php?view=view&code=<?php echo $result->CONFIRMATIONCODE; ?>" class="btn btn-primary btn-xs" ><i class="icon-edit">View</a>
@@ -74,8 +74,8 @@ foreach ($cur as $result) {
 		}
 
 	?>
-	
-	
+
+
 </td>
 
 <?php }
@@ -84,7 +84,7 @@ foreach ($cur as $result) {
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
-						
+
 
 						<div class="alert alert-info">Profile:</div>
 					</div>
@@ -92,17 +92,17 @@ foreach ($cur as $result) {
 					<form action="#"  method=
 					"post">
 						<div class="modal-body">
-					
-												
+
+
 								<div id="display">
-									
+
 										<p>ID : <div id="infoid"></div></p><br/>
 											Name : <div id="infoname"></div><br/>
 											Email Address : <div id="Email"></div><br/>
 											Gender : <div id="Gender"></div><br/>
 											Birthday : <div id="bday"></div>
 										</p>
-										
+
 								</div>
 						</div>
 
